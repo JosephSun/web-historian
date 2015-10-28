3aba9ac9a7c361526c0acfe36f1efddb6d61abbd
JosephSun <bazzkhurshid@yahoo.com>
2015-10-28 12:18:14 -0700

    added pseudo code

--- web/initialize.js
+++ web/initialize.js
@@ -1,23 +1,23 @@
 var fs = require('fs');
-
+/*Note: May need to change all of these syncs to async*/
 // Sync is ok here because this is called just once on startup.
 module.exports = function () {
   // if the archive folder doesn't exist, create it.
-  if (!fs.existsSync("./archives")) {
+  if (!fs.existsSync("./archives")) {//checks if file exists
     // We use fs.mkdirSync to create the folder
-    fs.mkdirSync("./archives");
+    fs.mkdirSync("./archives"); //Makes file
   }
 
   // if the file doesn't exist, create it.
   if (!fs.existsSync("./archives/sites.txt")) {
     // We use fs.openSync to create the file
-    var file = fs.openSync("./archives/sites.txt", "w");
-    fs.closeSync(file);
+    var file = fs.openSync("./archives/sites.txt", "w");/*This opens a file, w specifically opens a file for writing. If the file exists then it is truncated(replaced??)*/
+    fs.closeSync(file);//API is unclear
   }
 
   // if the folder doesn't exist, create it.
   if (!fs.existsSync("./archives/sites")) {
     // We use fs.mkdirSync to create the folder
-    fs.mkdirSync("./archives/sites");
+    fs.mkdirSync("./archives/sites");//Makes file
   }
 };
