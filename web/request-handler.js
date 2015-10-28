ac30872ef67fcfe124326eaafc6d32dbfb232238
JosephSun <bazzkhurshid@yahoo.com>
2015-10-28 15:47:30 -0700

    Added the request module.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,17 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var request = require("request");
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  request(url, function (error, response, body) {
+    if (!error) {
+      //Here is where we will add the downloaded page to the archive. The downloaded page should be body. We also add the url. 
+      //down
+        
+    } else {
+      console.log("We’ve encountered an error: " + error);
+    }
+  } );
   res.end(archive.paths.list);
 };
