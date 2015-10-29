7ed049ebc491fb8fffaa978036e53edff7676f6f
JosephSun <bazzkhurshid@yahoo.com>
2015-10-28 19:52:24 -0700

    Pseudo code

--- web/initialize.js
+++ web/initialize.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var archive = require('../helpers/archive-helpers');
+
 /*Note: May need to change all of these syncs to async*/
 // Sync is ok here because this is called just once on startup.
 module.exports = function () {
@@ -12,6 +14,7 @@ module.exports = function () {
   if (!fs.existsSync("./archives/sites.txt")) {
     // We use fs.openSync to create the file
     var file = fs.openSync("./archives/sites.txt", "w");/*This opens a file, w specifically opens a file for writing. If the file exists then it is truncated(replaced??)*/
+    //NOTE: Maybe we store the original index site here during initialization
     fs.closeSync(file);//API is unclear
   }
 
@@ -20,4 +23,7 @@ module.exports = function () {
     // We use fs.mkdirSync to create the folder
     fs.mkdirSync("./archives/sites");//Makes file
   }
+  console.log('*************************************************************rgsfbreegeregerge888*888***idegfieudf')
+  archive.readListOfUrls();
+  console.log('7&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&Apple juice')
 };
