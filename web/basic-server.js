a3a19e0a21c669113a5d635d05e4488ded2e0fbe
JosephSun <bazzkhurshid@yahoo.com>
2015-10-28 19:50:46 -0700

    Shifted some spaces

--- web/basic-server.js
+++ web/basic-server.js
@@ -2,15 +2,15 @@ var http = require("http");
 var handler = require("./request-handler");
 var initialize = require("./initialize.js");
 
-// Why do you think we have this here?
+// Why do you think we have this here?Note: We don't know. 
 // HINT: It has to do with what's in .gitignore
 initialize();
-
 var port = 8080;
 var ip = "127.0.0.1";
 var server = http.createServer(handler.handleRequest);
+//console.log('module.parent', module.parent);// this comes out to null. Meaning that we go through the else body. 
 
-if (module.parent) {
+if (module.parent) {//The api is not clear here. This is not a good api so far. Like for realz. 
   module.exports = server;
 } else {
   server.listen(port, ip);
